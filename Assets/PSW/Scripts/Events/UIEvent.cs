7cb4eb9587fc9b27f1821c487ea64ec1b95b775b
psw1305 <shshck5@gmail.com>
2024-01-03 10:02:20 +0900

    [➕Add] UI Event
    - UI Event Handler 추가
    - UI Bind 스크립트 수정

---
+++ Assets/PSW/Scripts/Events/UIEvent.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum UIEventType
+{
+    Click, PointerEnter, PointerExit
+}
+
+public static class UIEvent
+{
+    public static void SetEvent(this GameObject gameObject, UIEventType uiEventType, Action<PointerEventData> action)
+    {
+        UIEventHandler handler = Util.GetOrAddComponent<UIEventHandler>(gameObject);
+        handler.BindEvent(uiEventType, action);
+    }
+}
--- Assets/PSW/Scripts/Scene/UISceneTraning.cs
+++ Assets/PSW/Scripts/Scene/UISceneTraning.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UISceneTraning : UIScene
@@ -15,23 +16,33 @@ public class UISceneTraning : UIScene
     {
         base.Init();
 
+        SetButtons();
+        SetEvents();
+    }
+
+    private void SetButtons()
+    {
         SetUI<Button>();
         optionsBtn = GetUI<Button>("Btn_Options");
         gunManageBtn = GetUI<Button>("Btn_GunManage");
-        optionsBtn.onClick.AddListener(OpenOptionsPopop);
-        gunManageBtn.onClick.AddListener(OpenGunManagePopup);
+    }
+
+    private void SetEvents()
+    {
+        optionsBtn.gameObject.SetEvent(UIEventType.Click, OpenOptionsPopop);
+        gunManageBtn.gameObject.SetEvent(UIEventType.Click, OpenGunManagePopup);
     }
 
     #endregion
 
     #region Button Event
 
-    private void OpenOptionsPopop()
+    private void OpenOptionsPopop(PointerEventData eventData)
     {
         UI.ShowPopup<UIPopupOptions>(options);
     }
 
-    private void OpenGunManagePopup()
+    private void OpenGunManagePopup(PointerEventData eventData)
     {
         UI.ShowPopup<UIPopupGunManage>(gunManage);
     }
