fc28842423c05c730f959572b9b18abdd58a1854
psw1305 <shshck5@gmail.com>
2024-01-05 19:45:45 +0900

    [🛠️Fix] Item Tooltip
    - Tooltip 데이터 초기화
    - Tooltip 레이캐스트 수정
    - 총기 이미지 perserve aspect 적용

--- Assets/JEH/WeaponData/WeaponData_Gun.cs
+++ Assets/JEH/WeaponData/WeaponData_Gun.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
+
 [CreateAssetMenu(fileName = "NewWeapon", menuName = "Item/Weapon/Gun")]
 public class WeaponData_Gun : WeaponData
 {
     [Header("총 설정")]
-
-
     [SerializeField] private int _shotAtOnce; // 한번에 발사되는 총알 수.총알소모는 변수 관계없이 항상 1 
     [SerializeField] private float _spread; // 총알이 얼만큼 퍼져서 발싸되는지
-
-
     [SerializeField] GameObject _muzzleFlash;
 
     public int ShotAtOnce // 최소값 1
