2c11583ca7b8b5795af43c9676572c9e2eee3c2f
jyWooooo <wjy970423@gmail.com>
2024-01-08 12:09:10 +0900

    [💡Feature] Add UI key bind to InputManager
    [추가된 키 바인딩]
    - I 키 : 총기 관리 UI 열기
    - O 키 : 설정창 UI 열기
    - ESC 키 : 모든 팝업 닫기
    
    [추가된 기능]
    - PopUpUI가 초기화 시 InputManager의 ActionMap을 Switch하고 CursorRefresh 호출
    - UI ActionMap에서는 마우스 관련 플레이어 조작을 멈춤
    - CursorRefresh 메서드는 팝업이 열려있지 않다면 커서를 감추고, 열려있다면 커서를 보여줌. 또한 커서의 위치를 스크린 중앙으로 이동함.
      - OnApplicationFocus 메서드에서 CursorRefresh 호출

--- Assets/@Scripts/Managers/InputManager.cs
+++ Assets/@Scripts/Managers/InputManager.cs
@@ -12,10 +12,16 @@ public class InputManager : Singleton<InputManager>
     private InputAction _fastRunAction;
     private InputAction _walkAction;
     private InputAction _reloadAction;
+    private InputAction _gunManagementUIAction;
+    private InputAction _settingsOptionUIAction;
+    private InputAction _closeAllPopUpUIAction;
 
     private bool _fastRunPress;
     private bool _walkPress;
     private bool _firePress;
+
+    private GameObject _optionsUI;
+    private GameObject _gunManageUI;
 
     public Vector2 MouseDelta => _aimAction.ReadValue<Vector2>();
     public Vector2 PlayerMovement => _moveAction.ReadValue<Vector2>();
@@ -32,10 +38,21 @@ public class InputManager : Singleton<InputManager>
     {
         if (!base.Initialize()) return false;
 
+        _optionsUI = ResourceManager.Instance.GetCache<GameObject>("UI_Popup_Options");
+        _gunManageUI = ResourceManager.Instance.GetCache<GameObject>("UI_Popup_GunManage");
+
         _playerInput = gameObject.AddComponent<PlayerInput>();
         _playerInput.actions = ResourceManager.Instance.GetCache<InputActionAsset>("PlayerInputActions");
         _playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
-        _playerInput.SwitchCurrentActionMap("Player");
+        SwitchActionMap("Player");
+        RefreshCursor();
+
+        return true;
+    }
+
+    private void SwitchActionMap(string name)
+    {
+        _playerInput.SwitchCurrentActionMap(name);
 
         _moveAction = _playerInput.currentActionMap.FindAction("Move");
         _aimAction = _playerInput.currentActionMap.FindAction("Aim");
@@ -45,6 +62,9 @@ public class InputManager : Singleton<InputManager>
         _fastRunAction = _playerInput.currentActionMap.FindAction("FastRun");
         _walkAction = _playerInput.currentActionMap.FindAction("Walk");
         _reloadAction = _playerInput.currentActionMap.FindAction("Reload");
+        _gunManagementUIAction = _playerInput.currentActionMap.FindAction("GunManagementUI");
+        _settingsOptionUIAction = _playerInput.currentActionMap.FindAction("SettingsOptionUI");
+        _closeAllPopUpUIAction = _playerInput.currentActionMap.FindAction("CloseAllPopUpUI");
 
         // 키 상호작용
         // ex) 달리다가 정조준하면 달리기 취소. 걷다가 달리면 걷기 취소
@@ -56,9 +76,35 @@ public class InputManager : Singleton<InputManager>
         _walkAction.started += _ => { _walkPress = true; _fastRunPress = false; };
         _walkAction.canceled += _ => _walkPress = false;
         _reloadAction.started += _ => { _fastRunPress = false; _firePress = false; };
+        _gunManagementUIAction.started += _ => UIManager.Instance.ShowPopup<UIPopup>(_gunManageUI);
+        _settingsOptionUIAction.started += _ => UIManager.Instance.ShowPopup<UIPopup>(_optionsUI);
+        _closeAllPopUpUIAction.started += _ => UIManager.Instance.CloseAllPopupUI();
+    }
 
-        Cursor.visible = false;
+    public void OnOpenUI()
+    {
+        SwitchActionMap("UI");
+        RefreshCursor();
+    }
 
-        return true;
+    public void OnCloseUI()
+    {
+        SwitchActionMap("Player");
+        RefreshCursor();
+    }
+
+    private void RefreshCursor()
+    {
+        if (_playerInput.currentActionMap.name.Equals("Player"))
+            Cursor.visible = false;
+        else
+            Cursor.visible = true;
+        Mouse.current.WarpCursorPosition(new(Screen.width / 2, Screen.height / 2));
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+            RefreshCursor();
     }
 }
